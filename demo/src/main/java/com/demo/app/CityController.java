e6546ec09e5d57735d3d4a113b1423ec316910bd
saurabh515773 <awasthi.saurabh91@gmail.com>
2018-12-13 10:51:29 +0530

    Update CityController.java
    controller changes by saurabh

--- demo/src/main/java/com/demo/app/CityController.java
+++ demo/src/main/java/com/demo/app/CityController.java
@@ -1,27 +1,28 @@
 package com.demo.app;
 
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.io.File;
-
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import org.json.simple.parser.ParseException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.text.SimpleDateFormat;  
-import java.util.Date;  
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.couchbase.client.deps.com.fasterxml.jackson.databind.ObjectMapper;  
+import com.couchbase.client.deps.com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @RestController
 public class CityController {
-	@RequestMapping(value = "/fetch/{originCity}", method = RequestMethod.GET)
-	public @ResponseBody Object getCityName(@PathVariable String originCity) throws ParseException, java.text.ParseException {
+	
+
+	@RequestMapping(value = "/fetch/{originCity}/{destinationCity}", method = RequestMethod.GET)
+	public @ResponseBody Object getAllItineraries(@PathVariable String originCity , @PathVariable String destinationCity) throws ParseException, java.text.ParseException {
 		System.out.println("OriginCity-------->"+originCity);
 		AllCities Allcities = null;
 		
@@ -29,39 +30,13 @@ public class CityController {
 			File file = new ClassPathResource("city.json").getFile();
 			Allcities = new ObjectMapper().readValue(file, AllCities.class);
 			List<City> listofcity = Allcities.AllTheCities;
-			Itinerary result = null;
 			for(City x : listofcity)
 			{
 				if(x.getOriginCity().equals(originCity))
 				{
-					List<Itinerary> listOfItineraries = x.getItineraries();
-					int minConnection = Integer.MAX_VALUE;
-					long totalTime = Integer.MAX_VALUE;
-					SimpleDateFormat datetimeFormatter1 = new SimpleDateFormat( "yyyy-MM-dd hh:mm:ss");
-					Date deptTime = datetimeFormatter1.parse(x.getDepartureTime());
-					
-					for(Itinerary it : listOfItineraries)
-					{
-						Date arrTime = datetimeFormatter1.parse(it.getArrivalTime());
-						int diff = (int) (deptTime.getTime() - arrTime.getTime());
-						long minutes = TimeUnit.MILLISECONDS.toMinutes(diff);
-						/*if(totalTime > minutes) {
-							totalTime = minutes;
-						}*/
-						
-						if(minConnection >= Integer.parseInt(it.getConnectionNum()) && totalTime >= minutes) 
-						{
-							minConnection = Integer.parseInt(it.getConnectionNum());
-							totalTime = minutes;
-							result = it;
-						}
+					if(x.getDestinationCity().equals(destinationCity)) {
+						return x.getItineraries();
 					}
-					/*for(Itinerary it : listOfItineraries)
-					{
-						if(it.getConnectionNum()==minConnection)
-					}*/
-					
-								return result;
 				}
 			}
 			
@@ -72,11 +47,18 @@ public class CityController {
 		return null;
 	}
 
-	@RequestMapping(value = "/fetch/{originCity}/{destinationCity}/{connectionNum}", method = RequestMethod.GET)
-	public @ResponseBody Object getCityName(@PathVariable String originCity , @PathVariable String destinationCity , @PathVariable String connectionNum) throws ParseException, java.text.ParseException {
+	
+	@RequestMapping(value = "/fetch/{originCity}/{destinationCity}/fastest", method = RequestMethod.GET)
+	public @ResponseBody Object getFastestRoute(@PathVariable String originCity , @PathVariable String destinationCity) throws ParseException, java.text.ParseException {
+		
 		AllCities Allcities = null;
-		System.out.println("second service");
 		try {
+			Date departureDate = null;
+			Date arrivalDate = null;
+			long totalTimeTravel = Integer.MAX_VALUE;
+			int noOfHalts = Integer.MAX_VALUE;
+			String routeName = null;
+			
 			File file = new ClassPathResource("city.json").getFile();
 			Allcities = new ObjectMapper().readValue(file, AllCities.class);
 			List<City> listofcity = Allcities.AllTheCities;
@@ -87,21 +69,35 @@ public class CityController {
 				if(x.getOriginCity().equals(originCity))
 				{
 					if(x.getDestinationCity().equals(destinationCity)) {
+						//retreiving all the Itineraries in respect to origin and destination.
 						listOfItineraries = x.getItineraries();
+						
+						//converting the departure time to date format.
+						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+						departureDate = sdf.parse(x.getDepartureTime());
+						
 				//		System.out.println("before break in second service");
 						break;
 					}
 				}
-					
+				
 			}		
 			
-			for(Itinerary it : listOfItineraries)
+			for(Itinerary i : listOfItineraries)
 			{
-				if(it.getConnectionNum().equals(connectionNum)) {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+				arrivalDate = sdf.parse(i.getArrivalTime());
+				long diff = arrivalDate.getTime() - departureDate.getTime();
+				//System.out.println(diff);
+				if(diff<=totalTimeTravel && Integer.parseInt(i.getConnectionNum()) <= noOfHalts) {
 					//System.out.println("before return in second service with value");
-					return it.getPathId();
+					totalTimeTravel = diff;
+					noOfHalts = Integer.parseInt(i.getConnectionNum());
+					routeName = i.getPathId();
+					
 				}
 			}
+			return routeName;
 			
 		}catch(Exception e) {
 			e.printStackTrace();
